9520dca693b51ad99fefb81b749397921f2cb708
pscheri <52502371+pscheri@users.noreply.github.com>
2020-02-10 12:23:36 -0300

    Add files via upload

--- cloudwatch-to-s3.py
+++ cloudwatch-to-s3.py
@@ -1,25 +1,26 @@
 import boto3
 from datetime import datetime, timedelta
 import math
+import os
 
 # Variables
-logGroupName='LOG_GROUP' # Log Group name without first / (eg. 	aws-glue/crawlers)
-taskName='TASK_NAME'
-bucket='BUCKET_NAME'
+logGroupName = os.environ['LOG_GROUP'] # Log Group name without first / (eg. aws-glue/crawlers)
+taskName = os.environ['TASK_NAME']
+bucket = os.environ['BUCKET_NAME']
 
-client = boto3.client('logs')
+def lambda_handler(event, context):
+    client = boto3.client('logs')
 
+    now = datetime.utcnow() - timedelta(hours=1)
 
-now = datetime.utcnow() - timedelta(hours=1)
+    startOfHour = now.replace(minute=0, second=0, microsecond=0)
+    endOfHour = now.replace(minute=59, second=59, microsecond=999999)
 
-startOfHour = now.replace(minute=0, second=0, microsecond=0)
-endOfHour = now.replace(minute=59, second=59, microsecond=999999)
-
-response = client.create_export_task(
-    taskName=taskName,
-    logGroupName='/{}'.format(logGroupName),
-    fromTime=math.floor(startOfHour.timestamp() * 1000),
-    to=math.floor(endOfHour.timestamp() * 1000),
-    destination=bucket,
-    destinationPrefix='{}/{}/{}/{}/{}'.format(logGroupName,now.year,now.month,now.day,now.hour)
-)
+    response = client.create_export_task(
+        taskName=taskName,
+        logGroupName='/{}'.format(logGroupName),
+        fromTime=math.floor(startOfHour.timestamp() * 1000),
+        to=math.floor(endOfHour.timestamp() * 1000),
+        destination=bucket,
+        destinationPrefix='{}/{}/{}/{}/{}'.format(logGroupName,now.year,now.month,now.day,now.hour)
+    )
